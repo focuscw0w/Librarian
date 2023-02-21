0210914ee5d31a348986ff3f504ab142b2b82166
roncakjakub <51802574+roncakjakub@users.noreply.github.com>
2023-02-21 23:19:58 +0100

    Zmeneny styl mapy

--- src/views/LiveMap.vue
+++ src/views/LiveMap.vue
@@ -1,48 +1,48 @@
 <template>
   <div>
-    <FilterProduct v-if="activeFilterProduct" />
+    <FilterProduct v-if="activeFilterProduct"/>
 
     <SelectedBook
-      v-if="activeFilterProduct"
-      :bookData="book"
-      @hideFilterProduct="activeFilterProduct = false"
+        v-if="activeFilterProduct"
+        :bookData="book"
+        @hideFilterProduct="activeFilterProduct = false"
     />
     <div class="map-container">
       <GMapMap
-        ref="myMapRef"
-        :center="center"
-        :maxZoom="10"
-        :zoom="zoom"
-        :restriction="{
+          ref="myMapRef"
+          :center="center"
+          :maxZoom="10"
+          :zoom="zoom"
+          :restriction="{
           latLngBounds: slovakiaBounds,
           strictBounds: false,
         }"
-        map-type-id="terrain"
-        style="width: 100%; height: 100vh"
-        :class="{ dark: darkGoogleMap }"
-        @click="currentLibrary = null"
+          v-bind:options="mapStyle"
+          style="width: 100%; height: 100vh"
+          :class="{ dark: darkGoogleMap }"
+          @click="currentLibrary = null"
       >
         <GMapMarker
-          :key="index"
-          v-for="(m, index) in libraryLocation"
-          :position="m"
-          :clickable="true"
-          :icon="m.icon"
-          @click="showStore(index)"
-          @mouseover="toggleIcon(true, index)"
-          @mouseout="toggleIcon(false, index)"
+            :key="index"
+            v-for="(m, index) in libraryLocation"
+            :position="m"
+            :clickable="true"
+            :icon="m.icon"
+            @click="showStore(index)"
+            @mouseover="toggleIcon(true, index)"
+            @mouseout="toggleIcon(false, index)"
         >
           <GMapInfoWindow :opened="currentLibrary === index">
             <InfoWindow
-              :id="index"
-              :libraryName="m.libraryName"
-              :libraryCity="m.libraryCity"
-              :libraryStreet="m.libraryStreet"
-              :libraryPostCode="m.libraryPostCode"
-              :libraryHouseNumber="m.libraryHouseNumber"
-              :libraryTimeDirection="m.libraryTimeDirection"
-              :libraryTime="m.libraryTime"
-              :libraryStatus="m.libraryStatus"
+                :id="index"
+                :libraryName="m.libraryName"
+                :libraryCity="m.libraryCity"
+                :libraryStreet="m.libraryStreet"
+                :libraryPostCode="m.libraryPostCode"
+                :libraryHouseNumber="m.libraryHouseNumber"
+                :libraryTimeDirection="m.libraryTimeDirection"
+                :libraryTime="m.libraryTime"
+                :libraryStatus="m.libraryStatus"
             />
           </GMapInfoWindow>
         </GMapMarker>
@@ -50,15 +50,15 @@
 
       <div class="search-wrapper">
         <SearchBookForm
-          @addBook="book = $event"
-          @activeFilterProduct="activeFilterProduct = true"
-          @darkGoogleMap="darkGoogleMap = $event"
+            @addBook="book = $event"
+            @activeFilterProduct="activeFilterProduct = true"
+            @darkGoogleMap="darkGoogleMap = $event"
         />
       </div>
     </div>
   </div>
 
-  <Introduction v-if="$store.state.activeIntroduction" />
+  <Introduction v-if="$store.state.activeIntroduction"/>
 
   <div v-if="openRegister" class="dead-background"></div>
 </template>
@@ -97,13 +97,134 @@ export default {
       openedInfo: false,
       activeFilterProduct: false,
       zoom: 10,
-      center: { lat: 49.219631, lng: 18.74222 },
+      center: {lat: 49.219631, lng: 18.74222},
       slovakiaBounds: {
         north: 49.87026137082037,
         south: 47.65664527385194,
         east: 23.590802234193244,
         west: 16.313703742420486,
       },
+      mapStyle: {
+        styles: [
+          {
+            "featureType": "landscape.natural",
+            "elementType": "all",
+            "stylers": [
+              {
+                "visibility": "on"
+              }
+            ]
+          },
+          {
+            "featureType": "landscape.natural",
+            "elementType": "labels",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [
+              {
+                "visibility": "simplified"
+              }
+            ]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "labels.text",
+            "stylers": [
+              {
+                "visibility": "simplified"
+              }
+            ]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "labels.icon",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "poi.park",
+            "elementType": "all",
+            "stylers": [
+              {
+                "visibility": "on"
+              }
+            ]
+          },
+          {
+            "featureType": "poi.park",
+            "elementType": "labels",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [
+              {
+                "visibility": "simplified"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+              {
+                "visibility": "on"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "visibility": "on"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+              {
+                "visibility": "on"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "labels.icon",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [
+              {
+                "visibility": "simplified"
+              }
+            ]
+          }
+        ]
+      }
     };
   },
   methods: {
@@ -137,21 +258,21 @@ export default {
     },
     toggleIcon(statement, index) {
       this.libraryLocation[index].icon =
-        statement === true
-          ? require("../assets/icons/Point_book_active.png")
-          : require("../assets/icons/Point_book.png");
+          statement === true
+              ? require("../assets/icons/Point_book_active.png")
+              : require("../assets/icons/Point_book.png");
     },
   },
   async created() {
     this.$store.state.blurEffect =
-      localStorage.getItem("activeIntroduction") == null ? true : false;
+        localStorage.getItem("activeIntroduction") == null ? true : false;
 
     this.$store.state.activeIntroduction =
-      localStorage.getItem("activeIntroduction") == null ? true : false;
+        localStorage.getItem("activeIntroduction") == null ? true : false;
 
     await axios
-      .get("https://api.librarian.sk/api/libraries")
-      .then((response) => (this.libraries = response.data));
+        .get("https://api.librarian.sk/api/libraries")
+        .then((response) => (this.libraries = response.data));
 
     this.libraryLocation = this.libraries.map((library) => ({
       id: library.id,
